2918f9e8a5aac71b895874d8a6ee65ed40edfd7d
Anton <theshick@mail.ru>
2019-05-28 12:27:35 +0400

    Lesson-2 HW

--- Lesson_2/les_2_task_8.py
+++ Lesson_2/les_2_task_8.py
@@ -1,2 +1,28 @@
 # 8. Посчитать, сколько раз встречается определенная цифра в введенной последовательности чисел.
 # Количество вводимых чисел и цифра, которую необходимо посчитать, задаются вводом с клавиатуры.
+
+s = int(input('Введите количество чисел: '))
+num = ''
+for i in range(s):
+    n = input(f'Введите число номер {i+1}: ')
+    num += n
+n_search = int(input('Введите искомую цифру: '))
+count = 0
+num = int(num)
+
+
+def search(num, count, n_search):
+    if num:
+        if num % 10 == n_search:
+            count += 1
+        else:
+            pass
+        count = search(num // 10, count, n_search)
+        return count
+    else:
+        return count
+
+
+res = search(num, count, n_search)
+
+print(res)
--- Lesson_2/les_2_task_9.py
+++ Lesson_2/les_2_task_9.py
@@ -1,2 +1,26 @@
 # 9. Среди натуральных чисел, которые были введены, найти наибольшее по сумме цифр.
 # Вывести на экран это число и сумму его цифр.
+num_1 = int(input('Введите первое число: '))
+num_2 = int(input('Введите второе число: '))
+num_3 = int(input('Введите третье число: '))
+
+s = 0
+def search_max(num, s):
+    if num < 10:
+        return num
+    else:
+        s += num % 10 + search_max(num // 10, s)
+        return s
+
+s_1 = search_max(num_1, s)
+s_2 = search_max(num_2, s)
+s_3 = search_max(num_3, s)
+
+max_num = max(s_1, s_2, s_3)
+
+if max_num == s_1:
+    print(f'Наибольшее число - {num_1}, сумма цифр - {max_num}')
+elif max_num == s_2:
+    print(f'Наибольшее число - {num_2}, сумма цифр - {max_num}')
+else:
+    print(f'Наибольшее число - {num_3}, сумма цифр - {max_num}')
