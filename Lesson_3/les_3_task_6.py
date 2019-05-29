d880684d73924f5f372c92920f4bd297232267db
Anton <theshick@mail.ru>
2019-05-29 11:07:48 +0400

    Lesson-3

--- Lesson_3/les_3_task_6.py
+++ Lesson_3/les_3_task_6.py
@@ -1,2 +1,27 @@
 # 6. В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.
 # Сами минимальный и максимальный элементы в сумму не включать.
+import random
+
+len_arr = 10
+arr = [random.randint(0, 100) for _ in range(len_arr)]
+print(arr)
+min_i = 0
+min = arr[min_i]
+max_i = 0
+max = arr[max_i]
+for i, item in enumerate(arr):
+    if item <= min:
+        min = item
+        min_i = i
+    elif item > max:
+        max = item
+        max_i = i
+sum_ = 0
+if max_i > min_i:
+    for i in arr[min_i+1:max_i:]:
+        sum_ += i
+else:
+    for i in arr[min_i-1:max_i:-1]:
+        sum_ += i
+
+print(f'Сумма элементов между минимальным и максимальным равна: {sum_}')
--- Lesson_3/les_3_task_9.py
+++ Lesson_3/les_3_task_9.py
@@ -1,1 +1,27 @@
 # 9. Найти максимальный элемент среди минимальных элементов столбцов матрицы.
+import random
+
+ROW = 10
+COL = 10
+arr = [[random.randint(0, 100) for _ in range(COL)] for _ in range(ROW)]
+for row in arr:
+    for i in row:
+        print(f'{i:<3}', end='')
+    print()
+print('.............................................')
+arr = list(zip(*arr))
+
+min_arr = []
+for row in arr:
+    min = row[0]
+    for item in row:
+        if item <= min:
+            min = item
+    min_arr.append(min)
+
+max = min_arr[0]
+for item in min_arr:
+    if item >= max:
+        max = item
+
+print(f'Максимальный элемент среди минимальных элементов столбцов матрицы - {max}')
